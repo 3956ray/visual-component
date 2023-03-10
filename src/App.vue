3f47c914c573626db529d1f7a76e1688df80ada7
3956ray <3956ray@gmail.com>
2023-03-10 21:29:19 +0800

    use slots to put data in the component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <MaterialCardBox/>
+  <div id="app">
+    <MaterialCardBox>
+    <template v-slot:header>
+        <strong>Card Title</strong><br>
+        <span>Card Sub-Title</span>
+      </template>
+      <template v-slot:media>
+        <img src="https://via.placeholder.com/350x150">
+      </template>
+      <p>Main Section</p>
+      <template v-slot:action>
+        <button>Action Button</button>
+        <button>Action Button</button>
+      </template>
+    </MaterialCardBox>
   </div>
 </template>
 
@@ -15,7 +28,7 @@ export default {
 </script>
 
 <style>
-  body{
-    font-size: 14px;
-  }
+body {
+  font-size: 14px;
+}
 </style>
